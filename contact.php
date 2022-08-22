a956cbc8f26e340f746f339bf719ba10db08fddf
Rosa Pricillia Christine Sihombing <111721229+rosapricillia@users.noreply.github.com>
2022-08-22 10:53:42 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>PT. TOR GANDA</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/logotorganda.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            <h1 class="site-heading text-faded d-none d-lg-block">
+                <img src="assets/img/logotorganda.png">
+                <span class="site-heading-lower">PT. TOR GANDA</span>
+            </h1>
+        </header>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">PT. TOR GANDA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.php">Home</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.php">About</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.php">Products</a></li>
+                        <li class="nav-item px-lg-4 active"><a class="nav-link text-uppercase" href="contact.php">Contact</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="career.php">CAREER</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <section class="page-section cta">
+            <div class="container product-item">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <div class="cta-inner-1 bg-faded text-center rounded">
+                            <div class="google-maps">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.0455444348927!2d98.65901861444642!3d3.5770060513716317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031302a152b2441%3A0xffdf0e7154621cb4!2sPT.%20Torganda%20Kandir%20Medan.!5e0!3m2!1sid!2sid!4v1658978914470!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+                <section id="last">
+                    <div class="full bg-faded">
+                    <h3>CONTACT</h3>
+                    <div class="lt">
+                      <form class="form-horizontal"
+                            method="post" action="message.php">
+                        <div class="form-group">
+                          <div class="col-sm-12">
+                            <!-- name  -->
+                            <input
+                              type="text"
+                              class="form-control"
+                              id="name"
+                              placeholder="NAMA"
+                              name="nama"
+                              value=""
+                              required
+                            />
+                          </div>
+                        </div>
+             
+                        <div class="form-group">
+                          <div class="col-sm-12">
+                            <!-- email  -->
+                            <input
+                              type="email"
+                              class="form-control"
+                              id="email"
+                              placeholder="EMAIL"
+                              name="email"
+                              value=""
+                              required
+                            />
+                          </div>
+                        </div>
+             
+                        <!-- message  -->
+                        <textarea
+                          class="form-control"
+                          rows="8"
+                          placeholder="MESSAGE"
+                          name="message" required>
+                        </textarea>
+             
+                        <button
+                          class="btn btn-primary send-button"
+                          id="submit"
+                          type="submit"
+                          value="SEND">
+                          <i class="fa fa-paper-plane"></i>
+                          <span class="send-text">SEND</span>
+                        </button>
+                      </form>
+                      <!-- end of form -->
+                    </div>
+             
+                    <!-- Contact information -->
+                    <div class="rt">
+                      <ul class="contact-list">
+                        <li class="list-item">
+                          <i class="fa fa-map-marker fa-1x"></i>
+                            <span>
+                              Jl. Abdullah Lubis No.26 Kel. Babura, Kec. Medan Baru, Kota Medan 20153
+                            </span>
+                        </li>
+             
+                        <li class="list-item">
+                          <i class="fa fa-envelope fa-1x"></i>
+                              <a href="info@torganda.co.id" title="Send me an email">
+                                info@torganda.co.id</a>
+                        </li>
+             
+                        <li class="list-item">
+                          <i class="fa fa-phone fa-1x"></i>
+                            <span>
+                              (061) 415 1950
+                            </span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </section>
+            </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2-fix" href="https://web.facebook.com/torganda.co.id?_rdc=1&_rdr"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2-fix1" href="https://www.youtube.com/channel/UCb5unHWY1PhKkBjDPrkW1kQ"><i class="fab fa-youtube"></i></a>
+                    <a class="mx-2-fix2" href="https://www.instagram.com/torganda.co.id/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </section>
+        <footer class="footer text-faded text-center py-5">
+            <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
