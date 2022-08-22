3d8488f1d9a446ef7daef1560685d23ea1991283
Rosa Pricillia Christine Sihombing <111721229+rosapricillia@users.noreply.github.com>
2022-08-22 10:52:33 +0700

    Create products.php

---
+++ products.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>PT. TOR GANDA</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/logotorganda.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            <h1 class="site-heading text-faded d-none d-lg-block">
+                <img src="assets/img/logotorganda.png">
+                <span class="site-heading-lower">PT. TOR GANDA</span>
+            </h1>
+        </header>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">PT. TOR GANDA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.php">Home</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.php">About</a></li>
+                        <li class="nav-item px-lg-4 active"><a class="nav-link text-uppercase" href="products.php">Products</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="contact.php">Contact</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="career.php">CAREER</a></li>
+                    </ul>   
+                </div>
+            </div>
+        </nav>
+        <section class="page-section">
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex ms-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Minyak Kelapa Sawit Mentah</span>
+                                <span class="section-heading-lower">CRUDE PALM OIL</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/cpo1.jpg" alt="..." />
+                    <div class="product-item-description d-flex me-auto">
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Crude palm oil (CPO) adalah salah satu jenis minyak nabati yang paling banyak dikonsumsi oleh masyarakat dunia, yakni sekitar 40% dari seluruh jenis minyak nabati. Pemanfaatan minyak ini pun sangat beragam, terutama sebagai bahan pangan, industri <!–more–> kosmetik, industri kimia, industri pakan ternak, dan lain-lain.</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section">
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex me-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Kernel Sawit</span>
+                                <span class="section-heading-lower">PALM KERNEL</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/14.jpg" alt="..." />
+                    <div class="product-item-description d-flex ms-auto">
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Palm Kernel (bahasa Indonesia: kernel sawit) adalah sebutan lain dari inti atau biji buah kelapa sawit. Kernel sawit ini bisa dimakan dan juga bisa di olah menjadi palm kernel oil. Terdapat 2 macam minyak yang terbuat dari kelapa sawit yaitu pertama inti atau biji buah kelapa sawit yang dikenal dengan sebutan palm kernel oil. Dan yang kedua berasal dari daging buah kelapa sawit yang sudah melalui pemerasan dan perebusan yang dikenal dengan sebutan minyak sawit kasar.</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section">
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex ms-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Palm Kernel Shells</span>
+                                <span class="section-heading-lower">CANGKANG SAWIT</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/16.jpg" alt="..." />
+                    <div class="product-item-description d-flex me-auto">
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Cangkang sawit ( Palm Kernel Shells ) adalah sisa pecahan cangkang setelah biji sawit dikeluarkan dan dihancurkan di pabrik kelapa sawit (PKS).</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section clearfix">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">KANTOR PUSAT</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">JL. ABDULLAH LUBIS NO. 26, MEDAN</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">info@torganda.co.id</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">+62 853 6080 9064</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2-fix" href="https://web.facebook.com/torganda.co.id?_rdc=1&_rdr"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2-fix1" href="https://www.youtube.com/channel/UCb5unHWY1PhKkBjDPrkW1kQ"><i class="fab fa-youtube"></i></a>
+                    <a class="mx-2-fix2" href="https://www.instagram.com/torganda.co.id/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </section>
+        <footer class="footer text-faded text-center py-5">
+            <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
